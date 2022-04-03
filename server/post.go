7d09fc1745a5e4c60dd8a5253d5dd5d28a0b6de2
Fahmi Aziz <fahmiaziz884@gmail.com>
2022-04-03 17:52:12 +0700

    add body parser

--- server/post.go
+++ server/post.go
@@ -1,10 +1,26 @@
 package server
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Body struct {
+	User string `json:"user"`
+}
 
 func RoutePost(r *fiber.App){
 
 	r.Post("/api/register", func(c *fiber.Ctx) error {
-		return c.SendString("Im post")
+
+		body := new(Body)
+		c.BodyParser(body)
+
+		c.Set("Content-type", "application/json")
+		res, _ := json.Marshal(map[string]interface{}{
+			"message": "Hello " + body.User,
+		})
+		return c.Send(res)
 	})
 }
